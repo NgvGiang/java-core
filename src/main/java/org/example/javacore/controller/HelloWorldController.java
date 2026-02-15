ed34526f4737e2e927b0eca511082bebae2608b7
le chuck <hoanghl.22bi13170@usth.edu.vn>
2026-02-15 22:22:40 +0700

    lesson 1

--- src/main/java/org/example/javacore/controller/HelloWorldController.java
+++ src/main/java/org/example/javacore/controller/HelloWorldController.java
@@ -1,16 +1,29 @@
 package org.example.javacore.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.example.javacore.dto.UserDto;
+import org.springframework.web.bind.annotation.*;
+
+import java.math.BigDecimal;
 
 @RestController
 @RequestMapping("hello")
 public class HelloWorldController  {
+    //Path param (should only be used for 1 param in link(findByID))
+    @GetMapping("user/{userName}")
+    public String hello(@PathVariable String userName){
+        return "Hello "+ userName;
+    }
 
-    @GetMapping
-    public String hello(){
-        return "Hello World";
+    //Request Param (be used when there are multiple param in the link ...?userName=Chuck&age=21)
+    @GetMapping("userReqParam")
+    public String helloReqParam(@RequestParam String userName, int age){
+        return "Hello " + userName + age;
+    }
+
+    //Request Object (DTO = Data Transfer Object)
+    @GetMapping("userReqObj")
+    public String helloReqObj(@RequestBody UserDto userDto){
+        return "Hello " + userDto.getUserName() + userDto.getAge();
     }
 
 }
---
+++ src/main/java/org/example/javacore/dto/UserDto.java
@@ -0,0 +1,23 @@
+package org.example.javacore.dto;
+
+
+public class UserDto {
+    String userName;
+    Integer age;
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+}
